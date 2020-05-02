3aa4d014f9d16108b5c3887d912e9fe3c51fc724
carlsberg <amitjaiswal002@gmail.com>
2020-05-02 11:54:13 +0530

    UI Change

--- app/src/main/java/com/keplercollections/customtoastlibrary/MainActivity.java
+++ app/src/main/java/com/keplercollections/customtoastlibrary/MainActivity.java
@@ -1,8 +1,9 @@
 package com.keplercollections.customtoastlibrary;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void toast(View view) {
+        switch (view.getId()) {
+            case R.id.simple_toast:
+                ToastMessage.makeSimpleToast(getApplicationContext(),getString(R.string.simple_toast)).show();
+                break;
+            case R.id.warning_toast:
+                ToastMessage.makeWarningToast(getApplicationContext(),getString(R.string.warning_toast)).show();
+                break;
+            case R.id.error_toast:
+                ToastMessage.makeErrorToast(getApplicationContext(),getString(R.string.error_toast)).show();
+                break;
+            case R.id.success_toast:
+                ToastMessage.makeSuccessToast(getApplicationContext(),getString(R.string.success_toast)).show();
+                break;
+        }
     }
 }
